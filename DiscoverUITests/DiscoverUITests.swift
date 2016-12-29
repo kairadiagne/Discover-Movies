195488d824803479336f9dd1c22f154ca606a4b5
Kaira Diagne <diagnekaira@gmail.com>
2016-12-29 22:01:17 +0100

    Updated test cases

--- DiscoverUITests/DiscoverUITests.swift
+++ DiscoverUITests/DiscoverUITests.swift
@@ -13,60 +13,50 @@ class DiscoverUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // Setup fastlane
+        // Create the app
         let app = XCUIApplication()
-        setupSnapshot(app)
-        app.launch()
         
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        // Set up snapshot
+        setupSnapshot(app)
+   
+        // Stop in case of failure
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        // Launch the application
+        app.launch()
     }
     
     override func tearDown() {
         super.tearDown()
     }
     
+    // This test requires the app to be logged in 
+    // This is not possible in the test because the website has no accesabiliy support
     func testGenerateAppStoreScreenshots() {
-        // Start of app
+        // Go to menu
         let app = XCUIApplication()
+        app.navigationBars["Toplists"].buttons["Menu"].tap()
+        snapshot("menu-screenshot")
         
-        // Navigate to menu
-        app.navigationBars["Toplists"].children(matching: .button).element.tap()
-        snapshot("Menu")
-        
-        // Grab the table
+        // Go to watchlist
         let tablesQuery = app.tables
-        
-        // Sign in and go back to menu
-        app.tables.staticTexts["Sign in"].tap()
-        app.buttons["Sign in"].tap()
-        app.buttons["Done"].tap()
-        snapshot("SignIn")
-        app.navigationBars["Toplists"].children(matching: .button).element.tap()
-        
-        // Show watchlist
         tablesQuery.staticTexts["My watchlist"].tap()
+        snapshot("watchlist-screenshot", waitForLoadingIndicator: true)
+        
+        // Go back to menu
         app.navigationBars["Watchlist"].buttons["Menu"].tap()
-        snapshot("Watchlist")
-        
-        // Show favorites
+            
+        // Go to favorites
         tablesQuery.staticTexts["My favorite movies"].tap()
-        app.navigationBars["Favorites"].buttons["Menu"].tap()
-        snapshot("Favorites")
+        snapshot("favorites-snapshot", waitForLoadingIndicator: true)
         
-        // Go to first movie detail
-        tablesQuery.staticTexts["My favorite movies"].tap()
-        snapshot("MovieDetail-1")
-        
-        // Scroll down 
-        XCUIApplication().scrollViews.otherElements.containing(.staticText, identifier:"Her").element.swipeUp()
-        snapshot("MovieDetail-2")
-        
-        // Navigate back to menu
-        app.buttons["Back"].tap()
-        app.navigationBars["Favorites"].children(matching: .button).element.tap()
+        // Go to detail of first movie in list
+        app.tables.staticTexts["Her"].tap()
+        snapshot("detail-screenshot")
+    
+        // Scroll down
+        let herElement = app.scrollViews.otherElements.containing(.staticText, identifier:"Her").element
+        herElement.swipeUp()
+        snapshot("detail-screenshot-2")
     }
     
 }

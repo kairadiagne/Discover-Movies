d0199ab0dbae272eec5980b7dca0fb0eca3e8177
Kaira Diagne <diagnekaira@gmail.com>
2017-01-31 21:59:55 +0100

    Added CrossDissolveAnimatedTransitioning

---
+++ Discover/UserInterface/Common/CrossDissolveAnimatedTransitioning.swift
@@ -0,0 +1,59 @@
+//
+//  CrossDissolveAnimatedTransitioning.swift
+//  DiscoverMovies
+//
+//  Created by Kaira Diagne on 31-01-17.
+//  Copyright © 2017 Kaira Diagne. All rights reserved.
+//
+
+import UIKit
+
+class CrossDissolveAnimatedTransitioning: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    // MARK: - UIViewControllerAnimatedTransitioning
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        if let fromVC = transitionContext.viewController(forKey: .from),
+            let toVC = transitionContext.viewController(forKey: .to) {
+            
+            var fromView: UIView
+            var toView: UIView
+            
+            if let from = transitionContext.view(forKey: .from), let to = transitionContext.view(forKey: .to) {
+                fromView = from
+                toView = to
+            } else {
+                fromView = fromVC.view
+                toView = toVC.view
+            }
+            
+            let containerView = transitionContext.containerView
+            
+            fromView.frame = transitionContext.initialFrame(for: fromVC)
+            toView.frame = transitionContext.finalFrame(for: toVC)
+            toView.layoutSubviews()
+            
+            toView.alpha = 0
+            fromView.alpha = 1
+            
+            containerView.addSubview(toView)
+            
+            let transitionDuration = self.transitionDuration(using: transitionContext)
+            
+            // Animate
+            UIView.animate(withDuration: transitionDuration, delay: 0, options: [.curveLinear], animations: { 
+                toView.alpha = 1
+                fromView.alpha = 0
+            }, completion: { completed in
+                toView.alpha = 1
+                let transitionWasCancelled = transitionContext.transitionWasCancelled
+                transitionContext.completeTransition(!transitionWasCancelled)
+            })
+        }
+    }
+    
+}

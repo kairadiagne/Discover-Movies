8c16ac26d26c1ba918367f90f14cf08ce72ef004
Kaira Diagne <diagnekaira@gmail.com>
2019-12-15 11:32:34 +0100

    Update folder structure and add test case for list entity

---
+++ TMDbMovieKitTests/Core Data/Entities/ListTests.swift
@@ -0,0 +1,12 @@
+//
+//  ListTests.swift
+//  TMDbMovieKitTests
+//
+//  Created by Kaira Diagne on 15/12/2019.
+//  Copyright © 2019 Kaira Diagne. All rights reserved.
+//
+
+import XCTest
+
+final class ListTests: BaseTestCase {
+}
